bf51682574227da26c709f1357e658c6cd61bddd
JR Sonal <your_email@gmail.com>
2026-09-07 22:19:24 +0530

    againsd

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    # Run server locally on http://127.0.0.1:5000
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
